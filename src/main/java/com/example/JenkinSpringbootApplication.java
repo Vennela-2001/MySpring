2830f0721a7dc2c8df6e42e7b8f2601a62189027
Vennela Muchintala <Vennela.M@BRILLIO.COM>
2024-05-29 15:04:00 +0530

    My Spring Vennela

---
+++ src/main/java/com/example/JenkinSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinSpringbootApplication.class, args);
+	}
+
+}
